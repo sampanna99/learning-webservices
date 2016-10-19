9b18af627f086ecd84ac32e4a8e49d3878d7b430
sampanna pokhrel <sampanna99@gmail.com>
2016-10-18 22:41:27 -0500

    ussing session in web services

---
+++ CalculatorWebApplication/WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculatorWebApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            CalculatorService.CalculatorWebServiceSoapClient client = new CalculatorService.CalculatorWebServiceSoapClient();
+            int result = client.Add(Convert.ToInt32(txtFirstNumber.Text), Convert.ToInt32(txtSecondNumber.Text));
+            lblResult.Text = result.ToString();
+        }
+    }
+}
---
+++ WebServerDemonstration/CalculatorWebService.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Services;
+
+namespace WebServerDemonstration
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod(EnableSession =true)]
+        public int Add(int firstNumber, int secondNumber)
+        {
+            List<string> calculations;
+
+            if (Session["CALCULATIONS"] == null)
+            {
+                calculations = new List<string>();
+            }
+            else
+            {
+                calculations = (List<string>)Session["CALCULATIONS"];
+            }
+
+            string strRecentCalculation = firstNumber.ToString() + "+" + secondNumber.ToString() + " = " + (firstNumber + secondNumber).ToString();
+
+            calculations.Add(strRecentCalculation);
+            Session["CALCULATIONS"] = calculations;
+
+            return firstNumber + secondNumber;
+        }
+    }
+}
